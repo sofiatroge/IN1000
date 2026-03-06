27386410bfac009ad55a880d1f2f28841bf35c6c
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:52:48 +0100

    Oppgave 7

---
+++ pythontrix/Uke1/rektangel.py
@@ -0,0 +1,18 @@
+# a) Opprett to variabler, bredde og høyde. Bestem verdiene selv.
+bredde = 10
+høyde = 3
+
+# b) Skriv ut variablene til terminalen.
+print("Bredde er: ", bredde, "\nHøyde er: ", høyde)
+
+# c) Lag en tredje variabel, areal. Verdien skal være bredde ganger høyde.
+areal = bredde * høyde
+
+# d) Skriv ut arealet til terminalen.
+print ("Arealet blir: ", areal)
+
+
+# Output:
+# Bredde er:  10 
+# Høyde er:  3
+# Arealet blir:  30
