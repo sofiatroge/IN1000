edf98dca73ee23451d89930865fe36f2bdcde56f
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 15:30:48 +0100

    Oppgave 12

---
+++ pythontrix/Uke1/formatert_utskrift.py
@@ -0,0 +1,12 @@
+# a) Les om formatering av utskrift i læreboka s. 54-57 eller i Python-dokumentasjonen.
+
+# b) Skriv et program hvor du definerer to flyttallsvariabler: lengde og bredde. Gi disse variablene verdiene 10.101 og 3.843.
+
+a = 10.101
+b = 3.843
+
+# c) Skriv ut følgende setning ved å bruke formatering av utskrift og de to variablene:
+
+# Rektangelet er 10.1 cm langt og 3.8 cm bredt.
+
+print (f"Rektangelet er {a:.1f} cm langt og {b:.1f} cm bredt")
