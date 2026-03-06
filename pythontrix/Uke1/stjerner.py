a66eab4ab570fb7cfa39ed7e7ee822c4d393847e
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:43:58 +0100

    Oppgave 5

---
+++ pythontrix/Uke1/stjerner.py
@@ -0,0 +1,10 @@
+# Lag et Python-program som skriver ut tre linjer med stjerner "*", 
+# slik at det ser ut som en pyramide i terminalen. 
+# Kjør programmet og gjør endringer inntil du ser at programmet fungerer slik det skal.
+
+print("  *  \n *** \n*****")
+
+# Output:
+#   *  
+#  *** 
+# *****
