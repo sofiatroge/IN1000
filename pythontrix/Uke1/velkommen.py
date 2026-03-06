603c2b7a1df77fdb6cf02cb4257728dfc5585cec
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:22:12 +0100

    Update velkommen.py
    lagg til oppgavebesrivelse

--- pythontrix/Uke1/velkommen.py
+++ pythontrix/Uke1/velkommen.py
@@ -1,1 +1,9 @@
+# Filnavn: velkommen.py
+# a) Opprett en fil du kaller velkommen.py på din datamaskin.
+
+# b) Kopier koden nedefor til filen velkommen.py.
+
+# print("Velkommen til IN1000")
+# c) Naviger deg til riktig mappe i terminalen og kjør programmet.
+
 print("Velkommen til IN1000")
