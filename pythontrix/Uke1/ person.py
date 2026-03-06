7bbc2f22aad8244c5e3296b24c58826ad6652d28
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:28:23 +0100

    Update  person.py
    la til output fra terminal

--- pythontrix/Uke1/ person.py
+++ pythontrix/Uke1/ person.py
@@ -2,8 +2,12 @@
 print("Sofia +47 123456789 Slotte 1")
 
 # b) Kjør programmet fra a) og sjekk at det fungerer som det skal.
+# Sofia +47 123456789 Slotte 1
 
 # c) Skriv om programmet fra a), slik at det skriver navnet ditt, telefonnummer og adresse til terminal på forskjellige linjer.
 print("Sofia\n +47 123456789 \nSlotte 1")
 
 # d) Kjør programmet fra c) og sjekk at det fungerer som det skal.
+# Sofia
+# +47 123456789 
+# Slotte 1
