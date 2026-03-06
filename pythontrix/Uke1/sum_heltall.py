1a2650701d59ae2b679e0cbd07dfb9b885fbe083
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:46:34 +0100

    Oppgave 6

---
+++ pythontrix/Uke1/sum_heltall.py
@@ -0,0 +1,10 @@
+# a) Opprett to heltallsvariabler: a med verdien 4 og b med verdien 5.
+a = 4
+b = 5
+
+# b) Skriv ut summen av variablene. Begynn setningen med "Summen er: ".
+# Her skal du bruke variablene og ikke skrive tallene direkte.
+
+print("Summen er: ", a+b)
+
+# Output: Summen er:  9
