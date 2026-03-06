9d3cfe0b91ad41ad3767e848038ca1aeac022864
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 15:06:57 +0100

    Oppgave 9

---
+++ pythontrix/Uke1/sammmenlign.py
@@ -0,0 +1,19 @@
+# a) Opprett to heltallsvariabler, a og b. Bestem verdiene til variablene selv.
+a = 6 #4
+b = 12 #2
+
+
+# b) Sjekk om a er større enn b, og skriv ut til terminalen enten "a er større enn b" eller "a er ikke større enn b".
+if a > b:
+    print("a er større enn b")
+else:
+    print("a er ikke større enn b")
+
+# c) Ekstra: Endre verdiene til a og b, og inspiser at resultatet blir som forventet i alle tilfellene.
+# a = 4
+# b = 2
+# Output: a er større enn b
+
+# a = 6 
+# b = 12 
+# a er ikke større enn b
