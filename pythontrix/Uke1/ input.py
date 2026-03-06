e2dc129a0e889c3d21e2b621bcaaa96937986ccb
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 15:10:12 +0100

    Oppgave 10

---
+++ pythontrix/Uke1/ input.py
@@ -0,0 +1,7 @@
+# Skriv et program som spør brukeren "Hva er navnet ditt?" og skriver ut navnet til terminalen.
+navn = input("Hva er navnet ditt?")
+print ("Hei ", navn, " :) ")
+
+# Terminal:
+# Hva er navnet ditt?Lola
+# Hei  Lola  :) 
