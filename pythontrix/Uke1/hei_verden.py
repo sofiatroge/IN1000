331cf28bdd8ae3b304309b77f69b6e7c294b06a7
Sofia <89521792+sofiatroge@users.noreply.github.com>
2026-03-06 14:36:48 +0100

    oppgave fire

---
+++ pythontrix/Uke1/hei_verden.py
@@ -0,0 +1,24 @@
+# a) Skriv et program som skriver ut "Hei Verden!" til terminalen.
+print ("Hei Verden!")
+
+# b) Kjør programmet ditt og sjekk at alt fungerer.
+# output: Hei Verden!
+
+# c) Utvid programmet med en heltallsvariabel alder som har verdien 4.
+alder = 4
+
+# d) Skriv ut "Alder: " og verdien av alder til terminalen.
+print ("Hei Verden!", alder)
+
+# e) Kjør programmet ditt og sjekk at alt fungerer.
+# output: Hei Verden! 4
+
+# f) Legg inn følgende kommentar i programmet: "Nå endrer vi verdien av variabelen alder :".
+# g) Endre verdien i variabelen alder til din egen alder.
+alder = input("Nå endrer vi verdien av variabelen alder :")
+
+# h) Skriv ut en ny linje til terminalen med en passende tekst og den nye verdien til alder.
+print ("Den nye alderen er:", alder)
+
+# i) Kjør programmet ditt og sjekk at alt fungerer.
+# output: Den nye alderen er: 23
